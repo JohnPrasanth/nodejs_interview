58036ab5bdc7f1fb31fd8b738389ecf4359cb9d7
john <125994303+JohnPrasanth@users.noreply.github.com>
2023-06-28 19:16:15 +0530

    Update 1_user_register.js

--- 1_user_register.js
+++ 1_user_register.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const bcrypt = require("bcrypt");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
@@ -27,21 +26,19 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 // Add customer API call
 app.post("/users/", async (request, response) => {
-  const { username, name, gender,phoneNumber } = request.body;
-  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const { username='', gender='',phoneNumber='' } = request.body;
   // checking existing customer verification
+  if (username!=='' && gender!=='' && phoneNumber!==''){
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
     //adding new user
     const createUserQuery = `
       INSERT INTO 
-        user (username, name, password, gender, phone_number) 
+        user (username,  gender, phone_number) 
       VALUES 
         (
           '${username}', 
-          '${name}',
-          '${hashedPassword}', 
           '${gender}',
           '${phoneNumber}'
         )`;
@@ -52,4 +49,5 @@ app.post("/users/", async (request, response) => {
     response.status = 400;
     response.send("User already exists");
   }
+      }
 });
